e433b4ff819710241ab18bf26ed0c54ea16bd302
Keizh_still_beating <krishnamulay05@gmail.com>
2024-03-02 19:24:45 +0530

    fonew

--- src/component/Skills.jsx
+++ src/component/Skills.jsx
@@ -1,28 +1,28 @@
 function Skills() {
   return (
-    <div data-scroll-speed="2" className="SKILLS">
+    <div className="SKILLS">
       <h1 className="">Skills</h1>
 
-      <div data-scroll data-scroll-speed="2" id="react">
+      <div id="react">
         {" "}
         <img src="public\react.js-img.png" alt="" />
       </div>
 
-      <div data-scroll data-scroll-speed="1" id="tailwind">
+      <div id="tailwind">
         <img src="public\tailwind-image.jpg" alt="" />
       </div>
 
-      <div data-scroll-speed id="redux">
+      <div id="redux">
         {" "}
         <img src="public\redux-logo-landscape.png" alt="" />
       </div>
 
-      <div data-scroll-speed id="framer_motion">
+      <div id="framer_motion">
         {" "}
         <img id="framer_motion" src="public\FM.png" alt="" />
       </div>
 
-      <div data-scroll-speed id="allthree">
+      <div id="allthree">
         {" "}
         <img className="allthree" src="public\allthree.jpg" alt="" />
       </div>
